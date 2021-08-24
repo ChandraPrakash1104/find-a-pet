09e37c7ecd9d0de3837460f4d2666de981c621a9
Chandra Prakash <chandraprakash00504@gmail.com>
2021-08-24 14:49:19 +0530

    added styling in home page

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const MongoDBStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/adopt';
 
-
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
